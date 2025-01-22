aa87135ad670b306618334e0bea667783b67f4bb
adi11223353453 <adimer66@gmail.com>
2025-01-22 17:34:15 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,23 @@
+let sum = document.getElementById('ssds');
+
+let amount=0;
+
+function add(){
+    amount=amount+1;
+    ssds.innerHTML=amount;
+}
+
+function take(){
+    amount=amount-1;
+    ssds.innerHTML=amount;
+}
+
+function mul1(){
+    amount=amount*2
+    ssds.innerHTML=amount;
+}
+
+function mul2(){
+    amount=amount*0
+    ssds.innerHTML=amount;
+}
